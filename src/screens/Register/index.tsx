62e392f7043b404d48d6ad9301f5fcd50f3167c2
rodsjessica <jessirodrigues0501@gmail.com>
2022-07-06 14:59:03 -0300

    add TransactionTypeButton component

--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Container, Header, Title, Form, Fields} from './styles';
 import {Input} from '../../components/Form/Input';
 import {Button} from '../../components/Button';
+import {TransactionTypeButton} from '../../components/Form/TransactionTypeButton';
 
 export function Register() {
   return (
@@ -13,6 +14,9 @@ export function Register() {
         <Fields>
           <Input placeholder="Nome" />
           <Input placeholder="Preço" />
+
+          <TransactionTypeButton title="Income" type="up" />
+          <TransactionTypeButton title="Income" type="up" />
         </Fields>
 
         <Button title="Enviar" />
