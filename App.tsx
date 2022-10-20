2dcba7874b1adeaa1d157a89c7c5db8899d37f05
rodsjessica <jessirodrigues0501@gmail.com>
2022-10-20 17:25:36 -0300

    add configuration react navigation

--- App.tsx
+++ App.tsx
@@ -1,19 +1,18 @@
 import React from 'react';
 import {ThemeProvider} from 'styled-components';
 import {StatusBar} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {AppRoutes} from './src/routes/app.routes';
 
 import theme from './src/global/styles/theme';
-// import {Dashboard} from './src/screens/Dashboard';
-import {Register} from './src/screens/Register';
-// import {Category} from './src/screens/Category';
 
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
-      <StatusBar barStyle={'dark-content'} backgroundColor={'#5636D3'} />
-      {/* <Dashboard /> */}
-      <Register />
-      {/* <Category /> */}
+      <NavigationContainer>
+        <StatusBar barStyle={'dark-content'} backgroundColor={'#5636D3'} />
+        <AppRoutes />
+      </NavigationContainer>
     </ThemeProvider>
   );
 }
